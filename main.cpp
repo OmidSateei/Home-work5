b65ef94b640ec992cc4e135a7197798d27d8cb67
OmidSateei <153393617+OmidSateei@users.noreply.github.com>
2023-12-09 23:58:11 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,26 +3,22 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-	double salary, tax=0;
-	cout<<"enter the salary";
-	cin>> salary;
+	char ch;
+	cout<<"enter character";
+	cin>>"%c",&ch;
 	
-	if(salary<6000000){
-		tax=0;
-	} else if(salary>=6000000 && salary<8000000){
-		tax=salary *0.05;
-	} else if (salary>= 8000000 && salary<10000000){
-tax= salary *0.1;
-	} else if(salary >= 10000000 && salary < 14000000){
-tax= salary* 0.15;
-	} else if (salary>=14000000 && salary <18000000){
-tax=salary*0.20;
-	} else if (salary>= 18000000 && salary < 25000000){
-tax= salary * 0.25;
-	}else {
-tax=*0.35;
+	switch(ch) { 
+	case'a':
+		case'e':
+			case'o':
+				case'u':
+					case'i':
+						
+						cout<<"vowel\n";
+						break;
+						default:
+							cout<<"not vowel\n";
+							
 	}
-	double final salary= salart - tax;
-	cout<<"final salary after tax deduction:"<<final salary<<end;
-		return 0;
+	return 0;
 }
